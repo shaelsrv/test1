4baacb3f97b4cad73acd0a6bb68e9a69d05783dc
Sail Saurabh <shael.saurabh@gmail.com>
2020-04-01 11:16:09 -0500

    test
    a

---
+++ random.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {1..250}
+do
+	#echo "writing $i file"
+	#name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo ''
+	#echo $name
+	head -c 900KB /dev/random > "$i".c
+done
+
